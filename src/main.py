bdf27e1e87e00ef7c2451ad74f893817d6985602
kayiter23 <146771513+kayiter23@users.noreply.github.com>
2023-10-02 21:41:37 +0200

    Fix errors, refactor, add non-DL of non-existing files

--- src/main.py
+++ src/main.py
@@ -6,30 +6,48 @@ import argparse
 class Downloader:
     def __init__(self, model_name, quantity, headers, only_update):
         self.model_name = model_name
-        self.quantity = quantity
         self.folder_name = model_name
         self.headers = headers
         self.only_update = only_update
+        self.set_quantity(quantity)
         os.makedirs(self.folder_name, exist_ok=True)
 
     @staticmethod
     def check_num(no):
-        return {
-            range(9000, 10000): '/2000/',
-            range(8000, 9000): '/9000/',
-            range(7000, 8000): '/8000/',
-            range(6000, 7000): '/7000/',
-            range(5000, 6000): '/6000/',
-            range(4000, 5000): '/5000/',
-            range(3000, 4000): '/4000/',
-            range(2000, 3000): '/3000/',
-            range(1000, 2000): '/2000/',
-        }.get(no, '/1000/')
+        if no in range(9000, 10000):
+            return'/2000/'
+        elif no in range(8000, 9000):
+            return '/9000/'
+        elif no in range(7000, 8000):
+            return '/8000/'
+        elif no in range(6000, 7000):
+            return '/7000/'
+        elif no in range(5000, 6000):
+            return '/6000/'
+        elif no in range(4000, 5000):
+            return '/5000/'
+        elif no in range(3000, 4000):
+            return '/4000/'
+        elif no in range(2000, 3000):
+            return  '/3000/'
+        elif no in range(1000, 2000):
+            return '/2000/'
+        else:
+            return '/1000/'
 
     @staticmethod
     def video_exists(path, headers):
         r = requests.head(path, headers=headers)
         return r.status_code == requests.codes.ok
+
+    def set_quantity(self, quantity):
+        if not quantity:
+            r = requests.get("https://fapello.com/" + self.model_name + "/", headers=self.headers)
+            soup = BeautifulSoup(r.text, features="lxml")
+            q = soup.select_one("div#content a").get("href").split("/")[-2]
+            self.quantity = int(q)
+        else:
+            self.quantity = quantity
 
     def download_media(self):
         if not self.quantity:
@@ -48,6 +66,10 @@ class Downloader:
             file_name = complete_url[complete_url.rindex('/') + 1:]
             file_path = os.path.join(self.folder_name, file_name)
             
+            if downloaded_file.status_code != requests.codes.ok:
+                print("[ " + str(downloaded_file.status_code) + " ] ~> ", model_num+file_path)
+                continue
+
             if self.only_update and os.path.exists(file_path):
                 print("[ EXISTS ] ~> ", model_num+file_path)
                 break
@@ -73,6 +95,6 @@ if __name__ == '__main__':
 
     headers = {"User-Agent":"Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/118.0"}
     
-    downloader = Downloader(args.m, args.q, headers, args.u)
+    downloader = Downloader(args.model, args.quantity, headers, args.update)
     downloader.download_media()
 
